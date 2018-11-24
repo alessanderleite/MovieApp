59de9e210d9ea32c4ce843a50f5ba257b4797d09
alessanderleite <alessander.logistica@gmail.com>
2018-11-24 12:47:10 -0200

    images

--- app/src/main/java/com/example/alessander/movieapp/MainActivity.java
+++ app/src/main/java/com/example/alessander/movieapp/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.alessander.movieapp;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
