5db7dd22bdf898418995d1d36a570a3f8b38f6ae
alessanderleite <alessander.logistica@gmail.com>
2018-11-25 16:44:06 -0200

    writing the SettingsActivity.java methods

--- app/src/main/java/com/example/alessander/movieapp/SettingsActivity.java
+++ app/src/main/java/com/example/alessander/movieapp/SettingsActivity.java
@@ -1,13 +1,68 @@
 package com.example.alessander.movieapp;
 
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.preferences);
+        bindPreferenceSummaryToValue(findPreference("sortby"));
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.menu_settings, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+        String stringValue = newValue.toString();
+        if (preference instanceof ListPreference) {
+
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+
+            if (prefIndex >= 0) {
+
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }
+        else {
+
+            preference.setSummary(stringValue);
+        }
+        return false;
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+
+        preference.setOnPreferenceChangeListener(this);
+
+        //trigger listener with preferences current value
+        onPreferenceChange(preference, PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
     }
 }
