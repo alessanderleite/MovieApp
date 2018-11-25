e43678eb68ff3c453d45cefb2b42c47dc40e21a3
alessanderleite <alessander.logistica@gmail.com>
2018-11-24 23:36:42 -0200

    API key from The Movie DB. Implementing JSON

--- app/src/main/java/com/example/alessander/movieapp/MoviesFragment.java
+++ app/src/main/java/com/example/alessander/movieapp/MoviesFragment.java
@@ -17,6 +17,16 @@ import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -25,6 +35,8 @@ public class MoviesFragment extends Fragment {
     static int width;
     static ArrayList<String> posters;
     static boolean sortByPop;
+    static String API_KEY = "d0900c37c7422cd4ba21fbaa2e0c0fd8";
+
     public MoviesFragment() {
     }
 
@@ -113,15 +125,85 @@ public class MoviesFragment extends Fragment {
 
             }
         }
-        public String[] getPathsFromAPI(boolean sort) {
+        public String[] getPathsFromAPI(boolean sortbypop) {
 
-            String[] array = new String[15];
-            for(int i = 0; i<array.length;i++) {
+           while (true) {
 
-                array[i] = "/kqjL17yufvn9OVLyXYpvtyrFfak.jpg";
+               HttpURLConnection urlConnection = null;
+               BufferedReader reader = null;
+               String JSONResult;
+
+               try {
+                   String urlString = null;
+                   if (sortbypop) {
+
+                       urlString = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + API_KEY;
+
+                   }
+                   else {
+                       urlString = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=500&api_key=" + API_KEY;
+                   }
+
+                   URL url = new URL(urlString);
+                   urlConnection = (HttpURLConnection) url.openConnection();
+                   urlConnection.setRequestMethod("GET");
+                   urlConnection.connect();
+
+                   //Read the input stream into a String
+                   InputStream inputStream = urlConnection.getInputStream();
+                   StringBuffer buffer = new StringBuffer();
+                   if (inputStream == null) {
+
+                       return null;
+                   }
+                   reader = new BufferedReader(new InputStreamReader(inputStream));
+                   String line;
+                   while ((line = reader.readLine()) != null) {
+
+                       buffer.append(line + "\n");
+                   }
+                   JSONResult = buffer.toString();
+
+                   try {
+                       return getPathsFromJSON(JSONResult);
+                   } catch (JSONException e) {
+
+                       return null;
+                   }
+               } catch (Exception e) {
+
+                   continue;
+               } finally {
+                   if (urlConnection != null) {
+
+                       urlConnection.disconnect();
+                   }
+                   if (reader != null) {
+
+                       try {
+                           reader.close();
+                       } catch (final IOException e) {
+
+                       }
+                   }
+               }
+           }
+        }
+
+        public String[] getPathsFromJSON(String JSONStringParam) throws JSONException {
+
+            JSONObject JSONString = new JSONObject(JSONStringParam);
+
+            JSONArray moviesArray = JSONString.getJSONArray("results");
+            String[] result = new String[moviesArray.length()];
+
+            for (int i = 0; i < moviesArray.length(); i++) {
+
+                JSONObject movie = moviesArray.getJSONObject(i);
+                String moviePath = movie.getString("poster_path");
+                result[i] = moviePath;
             }
-            return array;
-
+            return result;
         }
     }
 }
