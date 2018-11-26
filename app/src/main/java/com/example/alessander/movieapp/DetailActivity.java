ddd6f89da29c967e2718396e723d47d2c5034ad8
alessanderleite <alessander.logistica@gmail.com>
2018-11-26 07:32:27 -0200

    calling id container

--- app/src/main/java/com/example/alessander/movieapp/DetailActivity.java
+++ app/src/main/java/com/example/alessander/movieapp/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.alessander.movieapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new DetailActivityFragment()).commit();
+
+        }
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.menu_detail, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
