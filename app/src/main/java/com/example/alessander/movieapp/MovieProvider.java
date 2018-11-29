abc3856b7658f62e71595dc4a8341d0ef93cd9d7
alessanderleite <alessander.logistica@gmail.com>
2018-11-29 07:22:56 -0200

    finish MovieProvider class with SQLite instructions

--- app/src/main/java/com/example/alessander/movieapp/MovieProvider.java
+++ app/src/main/java/com/example/alessander/movieapp/MovieProvider.java
@@ -1,13 +1,18 @@
 package com.example.alessander.movieapp;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 
-import java.net.URL;
 import java.util.HashMap;
 
 public class MovieProvider extends ContentProvider {
@@ -53,33 +58,142 @@ public class MovieProvider extends ContentProvider {
                     " youtube2, " +
                     " date);";
 
+    private static class DatabaseHelpler extends SQLiteOpenHelper {
+
+        public DatabaseHelpler(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL(CREATE_DB_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+            db.execSQL("DROP TABLE IF EXISTS " + MOVIES_TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
+
     @Override
     public boolean onCreate() {
-        return false;
+
+        Context context = getContext();
+        DatabaseHelpler dbHelper = new DatabaseHelpler(context);
+
+        db = dbHelper.getWritableDatabase();
+        return (db == null) ? false : true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(MOVIES_TABLE_NAME);
+
+        switch (uriMatcher.match(uri)) {
+            case MOVIES:
+                qb.setProjectionMap(MOVIES_PROJECTION_MAP);
+                break;
+
+            case MOVIES_ID:
+                qb.appendWhere( NAME + " = " + uri.getPathSegments().get(1));
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+
+        }
+        if (sortOrder == null || sortOrder == "") {
+
+            sortOrder = NAME;
+        }
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return c;
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+
+        switch (uriMatcher.match(uri)) {
+            case MOVIES:
+                return "vnd.android.cursor.dir/vnd.example.movies";
+
+            case MOVIES_ID:
+                return "vnd.android.cursor.item/vnd.example.movies";
+
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+
+        long rowID = db.insert(MOVIES_TABLE_NAME, "", values);
+        if (rowID > 0) {
+
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        throw new SQLException("Failed to add a record into " + uri);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+
+        int count = 0;
+
+        switch (uriMatcher.match(uri)) {
+            case MOVIES:
+                count = db.delete(MOVIES_TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case  MOVIES_ID:
+                String id = uri.getPathSegments().get(1);
+                count = db.delete( MOVIES_TABLE_NAME, NAME + " = " + id + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+
+        int count = 0;
+
+        switch (uriMatcher.match(uri)) {
+            case MOVIES:
+                count = db.update(MOVIES_TABLE_NAME, values, selection, selectionArgs);
+                break;
+
+            case MOVIES_ID:
+                count = db.update(MOVIES_TABLE_NAME, values, NAME + " = " + uri.getPathSegments().get(1) + (!TextUtils.isEmpty(selection) ? " AND (" +selection + ')' : ""), selectionArgs);
+
+                default:
+                    throw  new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return count;
+    }
+
+    public void clearTable() {
+
+        db.execSQL("DELETE FROM Movies");
     }
 }
